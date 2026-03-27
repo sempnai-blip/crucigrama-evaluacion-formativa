5b6a3b0a9dca704f0207d554159d0c7633d01138
Ian Garcia Beltran <iangarciabeltran@MacBook-Pro-de-Ian.local>
2026-03-26 21:38:49 -0600

    crossword grid version final

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-from crossword import answers
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/check", methods=["POST"])
-def check():
-    user_data = request.json
-    result = {}
-
-    for key in answers:
-        result[key] = user_data.get(key, "").upper() == answers[key]
-
-    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
