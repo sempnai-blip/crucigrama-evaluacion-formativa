9471249795435e457b9a93ad375d6947716010b4
Ian Garcia Beltran <iangarciabeltran@MacBook-Pro-de-Ian.local>
2026-03-26 21:16:13 -0600

    fix crossword answers import

---
+++ crossword.py
@@ -0,0 +1,16 @@
+answers = {
+    "3": "EVIDENCIAS",
+    "6": "COEVALUACION",
+    "9": "AUTOEVALUACION",
+    "11": "APRENDIZAJE",
+    "12": "AUTONOMIA",
+    "13": "REGULACION",
+
+    "1": "RETROALIMENTACION",
+    "2": "DESEMPENO",
+    "4": "INFORMACION",
+    "5": "FORMATIVA",
+    "7": "CRITERIOS",
+    "8": "PORTAFOLIO",
+    "10": "COMPETENCIA"
+}
