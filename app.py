d218ddc1cbc2de1a1d0110c24c8920c0afcb85eb
Ian Garcia Beltran <iangarciabeltran@MacBook-Pro-de-Ian.local>
2026-03-26 22:00:04 -0600

    crossword grid version final 3

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from crossword import GRID
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", grid=GRID)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- crossword.py
+++ crossword.py
@@ -1,16 +1,63 @@
-answers = {
-    "3": "EVIDENCIAS",
-    "6": "COEVALUACION",
-    "9": "AUTOEVALUACION",
-    "11": "APRENDIZAJE",
-    "12": "AUTONOMIA",
-    "13": "REGULACION",
+GRID = [
 
-    "1": "RETROALIMENTACION",
-    "2": "DESEMPENO",
-    "4": "INFORMACION",
-    "5": "FORMATIVA",
-    "7": "CRITERIOS",
-    "8": "PORTAFOLIO",
-    "10": "COMPETENCIA"
-}
+# fila 0
+[None,None,None,None,None,None,None,None,None,None,None,None,None,None,"R",None,None,None],
+
+# fila 1
+[None,None,None,None,None,None,None,None,None,None,None,None,None,None,"E",None,None,None],
+
+# fila 2
+[None,None,None,None,None,None,None,None,None,None,None,None,None,None,"T",None,None,None],
+
+# fila 3
+[None,None,"E","V","I","D","E","N","C","I","A","S",None,None,"R",None,None,None],
+
+# fila 4
+[None,None,None,None,None,None,None,None,None,None,None,"F",None,None,"O",None,None,None],
+
+# fila 5
+[None,None,None,None,None,None,None,None,None,None,None,"O",None,None,"A",None,None,None],
+
+# fila 6
+[None,None,None,None,None,None,None,None,None,None,None,"R",None,None,"L","C","O","E","V","A","L","U","A","C","I","O","N"],
+
+# fila 7
+[None,None,None,None,None,None,None,None,None,None,None,"M",None,None,"I",None,None,None],
+
+# fila 8
+[None,None,None,None,None,None,None,None,None,None,None,"A",None,None,"M",None,None,None],
+
+# fila 9
+[None,None,None,None,None,None,None,None,None,None,None,"T",None,None,"E",None,None,None],
+
+# fila 10
+[None,None,None,None,None,None,None,None,None,None,None,"I",None,None,"N",None,None,None],
+
+# fila 11
+[None,None,None,None,None,None,None,None,None,None,None,"V",None,None,"T",None,None,None],
+
+# fila 12
+[None,None,None,None,None,None,None,None,None,None,None,"A",None,None,"A",None,None,None],
+
+# fila 13
+[None,None,None,None,None,None,None,None,None,None,None,None,None,None,"C","R","I","T","E","R","I","O","S"],
+
+# fila 14
+["A","P","R","E","N","D","I","Z","A","J","E",None,None,None,"I",None,None,None],
+
+# fila 15
+[None,None,"A","U","T","O","N","O","M","I","A",None,None,None,"O",None,None,None],
+
+# fila 16
+["R","E","G","U","L","A","C","I","O","N",None,None,None,None,"N",None,None,None],
+
+# fila 17
+[None,None,"P","O","R","T","A","F","O","L","I","O",None,None,None,None,None,None],
+
+# fila 18
+[None,None,"C","O","M","P","E","T","E","N","C","I","A",None,None,None,None,None],
+
+# fila 19
+[None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
+
+]
